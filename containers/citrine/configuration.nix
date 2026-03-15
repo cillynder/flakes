d57703089247253842032ddfd7ce383e14587619
Cilly Leang <mini@cilly.moe>
2026-03-16 02:04:31 +1100

    containers/citrine: customise homepage and disable registrations

--- containers/citrine/configuration.nix
+++ containers/citrine/configuration.nix
@@ -3,16 +3,21 @@
   networking.firewall.allowedTCPPorts = [ 3000 ];
   networking.firewall.allowedUDPPorts = [ 3000 ];
 
+  systemd.tmpfiles.rules = [
+    "L+ /persist/forgejo/custom/templates - - - - ${./templates}"
+  ];
+
   services.forgejo = {
     enable = true;
     lfs.enable = true;
     settings = {
+      DEFAULT.APP_NAME = "Garden";
       server = {
         DOMAIN = "garden.lava.moe";
         ROOT_URL = "https://garden.lava.moe/";
         HTTP_PORT = 3000;
       };
-      service.DISABLE_REGISTRATION = false;
+      service.DISABLE_REGISTRATION = true;
     };
     stateDir = "/persist/forgejo";
   };
