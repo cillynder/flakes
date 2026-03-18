2d15fb3a5e216e8787ce5252591de959ee938ff4
Cilly Leang <mini@cilly.moe>
2026-03-18 22:09:04 +1100

    containers/fluorite: use jpg picture

--- containers/fluorite/configuration.nix
+++ containers/fluorite/configuration.nix
@@ -18,7 +18,7 @@
     settings = {
       shares.directories = [ "/binds/music/" ];
       soulseek.description = "🌸 | sv.sl@lava.moe | slskd";
-      soulseek.picture = "/var/lib/slskd/picture.png";
+      soulseek.picture = "/var/lib/slskd/picture.jpg";
     };
   };
 }
