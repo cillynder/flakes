220af6cf157e12bfe447658abfe5d90edf36d2dc
Cilly Leang <mini@cilly.moe>
2026-05-28 18:59:38 +1000

    alyssum/networking: enable wpa_supplicant

--- hosts/alyssum/networking.nix
+++ hosts/alyssum/networking.nix
@@ -2,6 +2,8 @@
   environment.etc."wpa_supplicant.conf".source = config.age.secrets.wpa_conf.path;
   networking = {
     useDHCP = true;
+    wireless.enable = true;
+
     interfaces.wlp1s0.useDHCP = false;
     interfaces.wlp1s0.ipv4.addresses = [{
       address = "192.168.1.167";
