0638cf6f5ff5e3c45d951bc514bcd3a941c9efb1
Cilly Leang <mini@cilly.moe>
2026-05-28 18:19:13 +1000

    alyssum/kernel: update

--- hosts/alyssum/kernel.nix
+++ hosts/alyssum/kernel.nix
@@ -1,10 +1,12 @@
-{ ... }: {
+{ config, lib, ... }: {
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
     };
-    initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-    initrd.kernelModules = [ "nvme" ];
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
   };
+  hardware.cpu.amd.updateMicrocode = true;
 }
