d99ec5e25b01568c0202605a5d511c8847616d43
Cilly Leang <mini@cilly.moe>
2026-06-17 01:05:23 +1000

    containers/fluorite: uuuuuuuuuuuuuuuuuuuuuuu

--- containers/fluorite/flake.nix
+++ containers/fluorite/flake.nix
@@ -47,7 +47,7 @@
       services.nginx.virtualHosts."${fqdn}" = {
         useACMEHost = "lava.moe";
         forceSSL = true;
-        locations."/".proxyPass = "http://[${clientTun}]:5030";
+        locations."/".proxyPass = "http://${clientTun}:5030";
         listenAddresses = listenAddr;
       };
 
@@ -55,7 +55,7 @@
       services.nginx.virtualHosts."${altfqdn}" = {
         useACMEHost = hostfqdn;
         forceSSL = true;
-        locations."/".proxyPass = "http://[${clientTun}]:5030";
+        locations."/".proxyPass = "http://${clientTun}:5030";
         listenAddresses = listenAddr;
       };
 
