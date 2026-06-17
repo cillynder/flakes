bc3269a814934ccd8dfa95462735125cdc5d5762
Cilly Leang <mini@cilly.moe>
2026-06-17 20:29:22 +1000

    alyssum/home.syncthing: create another instance

--- hosts/alyssum/home.syncthing.nix
+++ hosts/alyssum/home.syncthing.nix
@@ -1,21 +1,27 @@
-{ config, ... }: {
-  me.binds."/home/kujira/.config/syncthing" = "kujira/syncthing/config";
-  me.binds."/home/kujira/.local/state/syncthing" = "kujira/syncthing/state";
+{ config, lib, ... }:
+let
+  configOn = user: port: {
+    me.binds."/home/${user}/.config/syncthing" = "${user}/syncthing/config";
+    me.binds."/home/${user}/.local/state/syncthing" = "${user}/syncthing/state";
 
-  users.users.kujira = {
-    hashedPasswordFile = config.age.secrets.passwd.path;
-    isNormalUser = true;
-    linger = true;
-  };
-  home-manager.users.kujira = { ... }: {
-    home = {
-      username = "kujira";
-      homeDirectory = "/home/kujira";
-      stateVersion = "26.05";
+    users.users.${user} = {
+      hashedPasswordFile = config.age.secrets.passwd.path;
+      isNormalUser = true;
+      linger = true;
     };
-    services.syncthing = {
-      enable = true;
-      guiAddress = "[::]:8385";
+    home-manager.users.${user} = { ... }: {
+      home = {
+        username = "${user}";
+        homeDirectory = "/home/${user}";
+        stateVersion = "26.05";
+      };
+      services.syncthing = {
+        enable = true;
+        guiAddress = "[::]:${toString port}";
+      };
     };
   };
-}
+in lib.mkMerge [
+  (configOn "kujira" 8385)
+  (configOn "cilly" 8386)
+]
