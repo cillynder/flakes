d64c23ce975d341c07f80c39398a349ebb798956
Cilly Leang <mini@cilly.moe>
2026-06-19 10:20:00 +1000

    alyssum/restic: backup less frequently

--- hosts/alyssum/restic.nix
+++ hosts/alyssum/restic.nix
@@ -14,13 +14,12 @@
 
     paths = ["/flower"];
     timerConfig = {
-      # every 30mns
-      OnCalendar = "*-*-* *:00,30:00";
+      # every 6 hours
+      OnCalendar = "*-*-* 00,06,12,18:00:00";
       Persistent = true;
     };
     pruneOpts = [
-      "--keep-last 24"
-      "--keep-hourly 24"
+      "--keep-last 8"
       "--keep-daily 7"
       "--keep-weekly 5"
       "--keep-monthly 12"
