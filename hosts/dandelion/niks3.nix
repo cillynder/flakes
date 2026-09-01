d8d1411cf147c345fe759b27f1ea14e6d78d00fa
Cilly Leang <mini@cilly.moe>
2026-09-02 05:32:15 +1000

    dandelion/niks3: configure nginx caching correctly correctly

--- hosts/dandelion/niks3.nix
+++ hosts/dandelion/niks3.nix
@@ -51,7 +51,6 @@ in {
     keysZoneSize = "10m";
     maxSize = "1g";
     inactive = "24h";
-    path = "/var/cache/nginx/cache_niks";
   };
 
   services.nginx.virtualHosts."store.lava.moe" = {
@@ -60,9 +59,9 @@ in {
     locations."/".extraConfig = ''
       proxy_redirect https://lstore-nix.s3.us-west-004.backblazeb2.com/ https://store.s3-cf.lava.moe/;
       proxy_redirect https://s3.us-west-004.backblazeb2.com/ https://store.s3-cf.lava.moe/;
-      proxy_cache niks3cache;
+      proxy_cache /var/cache/nginx/niks3cache;
       proxy_cache_valid 200 24h;
-      proxy_cache_valid 404 5m;
+      proxy_cache_valid 404 1m;
       add_header X-Cache-Status $upstream_cache_status;
     '';
   };
