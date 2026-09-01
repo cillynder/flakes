5d6fc0fffdf3d5ccdba6f8ca6d27592aef19b795
Cilly Leang <mini@cilly.moe>
2026-09-02 01:05:19 +1000

    dandelion/niks3: configure proxy_redirect

--- hosts/dandelion/niks3.nix
+++ hosts/dandelion/niks3.nix
@@ -35,7 +35,6 @@ in {
       bucket = "lstore-nix";
       region = "us-west-004";
       useSSL = true;
-      publicUrl = "https://store.s3-cf.lava.moe";
     };
 
     nginx = {
@@ -49,5 +48,9 @@ in {
   services.nginx.virtualHosts."store.lava.moe" = {
     useACMEHost = "lava.moe";
     listenAddresses = [ "100.67.1.1" ];
+    locations."/".extraConfig = ''
+      proxy_redirect https://lstore-nix.s3.us-west-004.backblazeb2.com/ https://store.s3-cf.lava.moe/
+      proxy_redirect https://s3.us-west-004.backblazeb2.com/ https://store.s3-cf.lava.moe/
+    '';
   };
 }
