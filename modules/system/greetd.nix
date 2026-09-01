719f55842305ecbc8a1dc37d8dd442683c0b681a
Cilly Leang <mini@cilly.moe>
2026-09-01 19:26:07 +1000

    system/greetd: fix session

--- modules/system/greetd.nix
+++ modules/system/greetd.nix
@@ -3,14 +3,12 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --remember --asterisks --time --cmd 'zsh -c \"source $HOME/.config/zsh/.zshrc && Hyprland > $XDG_RUNTIME_DIR/Hyprland.out\"'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --remember --asterisks --time --cmd 'start-hyprland'";
         user = "greeter";
       };
 
       initial_session = {
-        command = "${pkgs.writeShellScript "launch.sh" ''
-          zsh -c "source $HOME/.config/zsh/.zshrc && Hyprland > \"$XDG_RUNTIME_DIR/Hyprland.out\""
-        ''}";
+        command = "start-hyprland";
         user = "rin";
       };
     };
