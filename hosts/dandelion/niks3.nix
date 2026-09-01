a1f3097fd701065e809e986c05ccd631b67dde93
Cilly Leang <mini@cilly.moe>
2026-09-02 05:13:17 +1000

    dandelion/niks3: enable nginx caching

--- hosts/dandelion/niks3.nix
+++ hosts/dandelion/niks3.nix
@@ -21,7 +21,7 @@ in {
     httpAddr = "127.0.0.1:5751";
     readProxy = {
       enable = true;
-      redirectTTL = "15m";
+      redirectTTL = "25h";
     };
 
     apiTokenFile = config.age.secrets."niks3_api_token".path;
@@ -51,6 +51,8 @@ in {
     locations."/".extraConfig = ''
       proxy_redirect https://lstore-nix.s3.us-west-004.backblazeb2.com/ https://store.s3-cf.lava.moe/;
       proxy_redirect https://s3.us-west-004.backblazeb2.com/ https://store.s3-cf.lava.moe/;
+      proxy_cache_valid 200 24h;
+      proxy_cache_valid 404 5m;
     '';
   };
 }
